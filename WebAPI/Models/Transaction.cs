1a3fa0131c68dcd44652503e3476ee241f1e48f6
Fernando Gruning <fernandojgruning@gmail.com>
2020-03-23 10:44:54 -0400

    Transaction: Add AccountId property

--- WebAPI/Models/Transaction.cs
+++ WebAPI/Models/Transaction.cs
@@ -4,28 +4,33 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebAPI.Models
 {
-    public abstract class Transaction
+  public abstract class Transaction
+  {
+
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public DateTime CreatedAt
     {
-
-        [Key]
-        public int Id { get; set; }
-
-        [Required]
-        public DateTime CreatedAt {
-            get {
-                return DateTime.Now;
-            }
-            private set { }
-        }
-        // public int IssuingAccount { get; set; }
-        // public int DestinationAccount { get; set; }
-        // public string Type { get; set; }
-        [Required]
-        [Column(TypeName = "decimal(10,2)")]
-        public decimal Amount { get; set; }
-
-        [Required]
-        [StringLength(100)]
-        public string Information { get; set; }
+      get
+      {
+        return DateTime.Now;
+      }
+      private set { }
     }
+    // public int IssuingAccount { get; set; }
+    // public int DestinationAccount { get; set; }
+    // public string Type { get; set; }
+    [Required]
+    [Column(TypeName = "decimal(10,2)")]
+    public decimal Amount { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Information { get; set; }
+
+    [Required]
+    public int AccountId { get; set; }
+  }
 }
