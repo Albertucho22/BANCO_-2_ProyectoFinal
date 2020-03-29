6150d217e934eee50a1b45d8c2601edc7db99b90
Albertucho22 <albertojosedominguez348@gmail.com>
2020-03-29 09:39:51 -0500

    See example conflict

--- WebAPI/Conflict/Example.cs
+++ WebAPI/Conflict/Example.cs
@@ -2,12 +2,6 @@ using System;
 
 namespace WebAPI.Conflict {
   public class Example {
-    public int Id { get; set; }
-    public string Information { get; set; }
-    public DateTime CreatedAt { get; private set; }
-
-    public Example() {
-      this.CreatedAt = DateTime.Now;
-    }
+    public int cantidad { get; set; }
   }
 }
