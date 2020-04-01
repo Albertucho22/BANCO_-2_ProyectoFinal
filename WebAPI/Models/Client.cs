807fd964e7151ec9149db25cc49bf1f80a010fce
Fernando Gruning <fernandojgruning@gmail.com>
2020-04-01 12:54:34 -0400

    Client: Add Accounts relationship

--- WebAPI/Models/Client.cs
+++ WebAPI/Models/Client.cs
@@ -1,48 +1,48 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace WebAPI.Models
-{
-    public class Client
-    {
-        [Key]
-        public int Id { get; set; }
+namespace WebAPI.Models {
+  public class Client {
+    [Key]
+    public int Id { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        [Display(Name = "First Name")]
+    [Required]
+    [StringLength(50)]
+    [Display(Name = "First Name")]
 
-        public string FirstName { get; set; }
+    public string FirstName { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        [Display(Name = "Last Name")]
-        public string LastName { get; set; }
-        // public string FullName { get { return FirstName + " " + LastName;} }
+    [Required]
+    [StringLength(50)]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+    // public string FullName { get { return FirstName + " " + LastName;} }
 
-        [Required]
-        [StringLength(50)]
-        public string UserName { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string UserName { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        public string Password { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string Password { get; set; }
 
-        [Required]
-        [EmailAddress]
-        [StringLength(50)]
-        public string Email { get; set; }
+    [Required]
+    [EmailAddress]
+    [StringLength(50)]
+    public string Email { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        public string NationalId { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string NationalId { get; set; }
 
 
-        public DateTime CreatedAt { get; private set; }
+    public DateTime CreatedAt { get; private set; }
 
-        public Client()
-        {
-            CreatedAt = DateTime.Now;
-        }
+    public virtual List<Account> Accounts { get; set; }
+
+    public Client() {
+      CreatedAt = DateTime.Now;
     }
+  }
 }
