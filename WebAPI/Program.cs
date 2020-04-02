714530951336546e15518861d97ca15070dde305
Albertucho22 <albertojosedominguez348@gmail.com>
2020-04-02 18:24:14 -0500

    Implement log4net

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -11,8 +15,12 @@ namespace WebAPI
 {
     public class Program
     {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         public static void Main(string[] args)
         {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
             CreateHostBuilder(args).Build().Run();
         }
 
