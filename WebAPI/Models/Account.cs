7d9b00d6562dffe41d9394ce5f577197f6b77005
Fernando Gruning <fernandojgruning@gmail.com>
2020-04-02 15:30:44 -0400

    Account: minor UpdateBalance refactor

--- WebAPI/Models/Account.cs
+++ WebAPI/Models/Account.cs
@@ -29,10 +29,11 @@ namespace WebAPI.Models {
     public void UpdateBalance(decimal amount) {
       // refactor method to accept a Transaction &
       // do operation based on Transaction derived Type
-      if (this.Balance + amount < 0) {
+      decimal newBalance = this.Balance + amount;
+      if (newBalance < 0) {
         throw new Exception("Account does not have sufficient funds.");
       }
-      this.Balance += amount;
+      this.Balance = newBalance;
     }
   }
 }
