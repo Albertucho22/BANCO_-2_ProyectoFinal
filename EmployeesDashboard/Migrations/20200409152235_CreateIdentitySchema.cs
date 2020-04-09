46f1f56f698f2966a244b6c646af84588d6af5fc
Fernando Gruning <fernandojgruning@gmail.com>
2020-04-09 11:24:58 -0400

    EmployeesDashboard: Change to SQL Server

--- EmployeesDashboard/Migrations/20200409152235_CreateIdentitySchema.cs
+++ EmployeesDashboard/Migrations/20200409152235_CreateIdentitySchema.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace EmployeesDashboard.Data.Migrations
+namespace EmployeesDashboard.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
@@ -51,7 +51,7 @@ namespace EmployeesDashboard.Data.Migrations
                 columns: table => new
                 {
                     Id = table.Column<int>(nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
+                        .Annotation("SqlServer:Identity", "1, 1"),
                     RoleId = table.Column<string>(nullable: false),
                     ClaimType = table.Column<string>(nullable: true),
                     ClaimValue = table.Column<string>(nullable: true)
@@ -72,7 +72,7 @@ namespace EmployeesDashboard.Data.Migrations
                 columns: table => new
                 {
                     Id = table.Column<int>(nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
+                        .Annotation("SqlServer:Identity", "1, 1"),
                     UserId = table.Column<string>(nullable: false),
                     ClaimType = table.Column<string>(nullable: true),
                     ClaimValue = table.Column<string>(nullable: true)
@@ -161,7 +161,8 @@ namespace EmployeesDashboard.Data.Migrations
                 name: "RoleNameIndex",
                 table: "AspNetRoles",
                 column: "NormalizedName",
-                unique: true);
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
 
             migrationBuilder.CreateIndex(
                 name: "IX_AspNetUserClaims_UserId",
@@ -187,7 +188,8 @@ namespace EmployeesDashboard.Data.Migrations
                 name: "UserNameIndex",
                 table: "AspNetUsers",
                 column: "NormalizedUserName",
-                unique: true);
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
