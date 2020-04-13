9f72673a0a04a416055ab3b1c930611002cc4aae
Fernando Gruning <fernandojgruning@gmail.com>
2020-04-13 13:35:19 -0400

    Add Swagger for API documentation

--- WebAPI/Controllers/HomeController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace WebAPI.Controllers {
-  public class HomeController : Controller {
-    public IActionResult Index() {
-      return View();
-    }
-  }
-}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -12,21 +12,18 @@ using Microsoft.Extensions.Hosting;
 using WebAPI.Data;
 using WebAPI.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
-namespace WebAPI
-{
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
-    {
+namespace WebAPI {
+  public class Startup {
+    public Startup(IConfiguration configuration) {
       Configuration = configuration;
     }
 
     public IConfiguration Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
+    public void ConfigureServices(IServiceCollection services) {
       services.AddDbContext<Core2DbContext>(options => options.UseLazyLoadingProxies().UseSqlServer(Configuration.GetConnectionString("Core2Db")));
 
       services.AddScoped<ClientService>();
@@ -38,18 +35,18 @@ namespace WebAPI
       // services.AddControllersWithViews();
       services.AddMvc().AddNewtonsoftJson().SetCompatibilityVersion(CompatibilityVersion.Latest);
       services.AddControllersWithViews();
+
+      services.AddSwaggerGen(c => {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Core 2 Web API", Version = "v1" });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
 
-      if (env.IsDevelopment())
-      {
+      if (env.IsDevelopment()) {
         app.UseDeveloperExceptionPage();
-      }
-      else
-      {
+      } else {
         // app.UseExceptionHandler("/Home/Error");
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
@@ -57,12 +54,18 @@ namespace WebAPI
       app.UseHttpsRedirection();
       // app.UseStaticFiles();
 
+      app.UseSwagger();
+
+      app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Core 2 Web API v1");
+        c.RoutePrefix = string.Empty;
+      });
+
       app.UseRouting();
 
       app.UseAuthorization();
 
-      app.UseEndpoints(endpoints =>
-      {
+      app.UseEndpoints(endpoints => {
         endpoints.MapControllerRoute(
           name: "default",
           pattern: "{controller=Home}/{action=Index}/{id?}");
