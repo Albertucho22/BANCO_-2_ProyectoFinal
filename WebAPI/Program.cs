9b5508bc7ac2a190435f9619b49a7d04e2d6d1b6
Fernando Gruning <fernandojgruning@gmail.com>
2020-04-13 16:46:28 -0400

    Stub commit

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -11,24 +11,20 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace WebAPI
-{
-    public class Program
-    {
-        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-        public static void Main(string[] args)
-        {
-            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
-            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+namespace WebAPI {
+  public class Program {
+    private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+    public static void Main(string[] args) {
+      var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+      XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
 
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+      CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
