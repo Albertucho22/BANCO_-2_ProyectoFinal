5624b4c7e5b141c5603d0618756617c32265c4d7
Fernando Gruning <fernandojgruning@gmail.com>
2020-04-15 11:19:47 -0400

    EmployeesDashboard: Format LoanCreateModel props

--- EmployeesDashboard/Models/LoanCreateModel.cs
+++ EmployeesDashboard/Models/LoanCreateModel.cs
@@ -1,12 +1,22 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EmployeesDashboard.Models {
   public class LoanCreateModel {
+    [Display(Name = "Interest Rate (Yearly)")]
     public float YearlyInterestRate { get; set; }
+
+    [Display(Name = "Duration (Months)")]
     public int DurationInMonths { get; set; }
+
     [Column(TypeName = "decimal(10,2)")]
+    [Display(Name = "Total Amount")]
     public decimal TotalAmount { get; set; }
+
+    [Display(Name = "Account ID")]
     public int AccountId { get; set; }
+
+    [Display(Name = "Information")]
     public string Information { get; set; }
   }
 }
